40ef69f608078ae9cd0d4bc592086fe696c681e5
concat(Gurias) <148577944+concatgurias@users.noreply.github.com>
2023-10-24 00:26:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -73,30 +73,3 @@ function shuffle() {
 shuffle();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-function toggleFullscreen() {
-  if (!document.fullscreenElement) {
-      // Verifica se o método de tela cheia está disponível e o utiliza, com prefixos para suporte do navegador
-      if (document.documentElement.requestFullscreen) {
-          document.documentElement.requestFullscreen();
-      } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
-          document.documentElement.mozRequestFullScreen();
-      } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-          document.documentElement.webkitRequestFullscreen();
-      } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
-          document.documentElement.msRequestFullscreen();
-      }
-  } else {
-      // Sai do modo de tela cheia se já estiver nele
-      if (document.exitFullscreen) {
-          document.exitFullscreen();
-      } else if (document.mozCancelFullScreen) { /* Firefox */
-          document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-          document.webkitExitFullscreen();
-      } else if (document.msExitFullscreen) { /* IE/Edge */
-          document.msExitFullscreen();
-      }
-  }
-}
-toggleFullscreen();
